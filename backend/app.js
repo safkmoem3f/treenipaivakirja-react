dd1ff5038fe9fc991007b434b984b6520f9d50e9
Melinda Backström <melinda.backstrom@windowslive.com>
2021-05-05 18:10:01 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,18 +10,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-var whitelist = ['https://treenipaivakirja.netlify.app']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(cors(corsOptions))
+app.all(cors())
 
 app.use('/user', userLogin);
 app.use('/result', results);
