27df33210f34dd2204881e55c307dcad4babc9c2
Melinda Backström <melinda.backstrom@windowslive.com>
2021-05-05 17:24:39 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.options((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 
     'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Methos', 'GET, POST, PATCH, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
     next();
 });
 
