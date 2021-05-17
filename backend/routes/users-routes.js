e14978b72f4ae3c3010a523a6afcee8ea8a5fef8
Melinda Backström <69964387+safkmoem3f@users.noreply.github.com>
2021-05-17 12:57:31 +0300

    Delete backend directory

--- backend/routes/users-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const usersControllers = require('../controllers/user-controllers');
-
-const router = express.Router();
-
-router.post('/login', usersControllers.login);
-router.post('/register', usersControllers.signup);
-
-module.exports = router;
